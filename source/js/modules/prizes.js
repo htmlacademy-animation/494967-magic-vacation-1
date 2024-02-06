797c29fd766a476081fadb07f9ffc70b962d0787
Анастасия Ратникова <fulldevop@mail.ru>
2024-02-06 19:24:26 +0300

    Реализовываем анимацию иллюстрации второстепенного приза

---
+++ source/js/modules/prizes.js
@@ -0,0 +1,6 @@
+export default () => {
+  setTimeout(() => {
+    document.getElementById('j-prizes-item-1').classList.add('translate');
+    document.getElementById('j-prizes-item-2').classList.add('active');
+  }, 3550);
+};

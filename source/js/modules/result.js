a14f4127e12320cbf2e03989769fc4a0087f008c
Анастасия Ратникова <fulldevop@mail.ru>
2024-02-16 23:20:28 +0300

    Реализовываем анимацию сцены с моржом

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -1,4 +1,10 @@
+import SeaCalfWinResult from "../canvas/sea-calf-canvas-animation";
+
 export default () => {
+  /* const resultWinCanvas = new SeaCalfWinResult();
+  const scriptRunResult = {
+    result: resultWinCanvas
+  };*/
   let showResultEls = document.querySelectorAll(`.js-show-result`);
   let results = document.querySelectorAll(`.screen--result`);
   if (results.length) {
@@ -9,13 +15,14 @@ export default () => {
           el.classList.remove(`screen--show`);
           el.classList.add(`screen--hidden`);
         });
-        document.getElementById('game').style['z-index'] = 1;
-        document.getElementById('constantScreen').style['z-index'] = 0;
+        document.getElementById(`game`).style[`z-index`] = 1;
+        document.getElementById(`constantScreen`).style[`z-index`] = 0;
         let targetEl = [].slice.call(results).filter(function (el) {
           return el.getAttribute(`id`) === target;
         });
         targetEl[0].classList.add(`screen--show`);
         targetEl[0].classList.remove(`screen--hidden`);
+        resultScreenHandler(target);
       });
     }
 
@@ -29,6 +36,12 @@ export default () => {
         document.getElementById(`messages`).innerHTML = ``;
         document.getElementById(`message-field`).focus();
       });
+    }
+  }
+
+  function resultScreenHandler(target) {
+    if (target === `result`) {
+      const resultWinCanvas = new SeaCalfWinResult();
     }
   }
 };
